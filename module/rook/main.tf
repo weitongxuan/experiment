29a1ab05f41cf7a61129530cd086742a84d56962
weitongxuan <hellosimon27@gmail.com>
2021-08-09 00:34:44 +0800

    add storage

--- module/rook/main.tf
+++ module/rook/main.tf
@@ -12,15 +12,24 @@ resource "helm_release" "rook" {
   depends_on = [
     kubernetes_namespace.rookNamespace
   ]
+  wait = true
+  wait_for_jobs = true
 }
 
-resource "helm_release" "rook" {
-  name       = "rook-ceph"
+resource "helm_release" "rookCluster" {
+  name       = "rook-ceph-cluster"
   repository = "https://charts.rook.io/release"
   chart      = "rook-ceph-cluster"
   namespace = "rook-ceph"
 
   values = [
-    "${file("values.yaml")}"
+    "${file("module/rook/values.yaml")}"
   ]
+
+  depends_on = [
+    kubernetes_namespace.rookNamespace,
+    helm_release.rook
+  ]
+  wait = true
+  wait_for_jobs = true
 }
